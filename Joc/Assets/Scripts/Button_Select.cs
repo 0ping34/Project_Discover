3af2c912dd743b23b1abd95f37e6ee44cb7b9b7c
0ping34 <129680510+0ping34@users.noreply.github.com>
2023-04-03 18:12:54 +0300

    Update Main Menu

---
+++ Joc/Assets/Scripts/Button_Select.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Select : MonoBehaviour
+{
+    public void Press()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
